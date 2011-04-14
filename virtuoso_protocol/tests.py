74b3d6e1df81ffaa20bddcd9a1f8288dfe541b7a
Christoph Burgmer <christoph@nwebs.de>
2011-04-14 22:53:12 +0200

    Use plug-in unit tests as shipped with new SuRF

--- virtuoso_protocol/tests.py
+++ virtuoso_protocol/tests.py
@@ -6,20 +6,20 @@ from unittest import TestCase
 
 import surf
 from surf.query import a, select
-from surf.rdf import Literal
+from surf.rdf import URIRef
+from surf.test.plugin import PluginTestMixin
 
 ENV = 'SURF_SPARQL_TEST'
 ENDPOINT = (os.environ[ENV] if ENV in os.environ and os.environ[ENV].strip()
                             else "http://localhost:8890/sparql")
 
-class TestVirtuosoProtocol(TestCase):
-    """ Tests for virtuoso_protocol plugin. """
+class VirtuosoProtocolTestMixin(object):
     CONTEXT = "http://github.com/nwebs/surf.virtuoso_protocol#surf_test_graph_dummy2"
 
-
-    def _get_store_session(self, use_default_context=True):
+    def _get_store_session(self, use_default_context = True):
         """ Return initialized SuRF store and session objects. """
 
+        # maybe we can mock SPARQL endpoint.
         kwargs = {"reader": "virtuoso_protocol",
                   "writer" : "virtuoso_protocol",
                   "endpoint" : ENDPOINT,
@@ -34,8 +34,22 @@ class TestVirtuosoProtocol(TestCase):
         session = surf.Session(store)
 
         # Fresh start!
-        store.clear(self.CONTEXT)
+        store.clear("http://surf_test_graph/dummy2")
+        store.clear(URIRef("http://my_context_1"))
+        store.clear(URIRef("http://other_context_1"))
+        store.clear()
 
+        return store, session
+
+class StandardPluginTest(TestCase, VirtuosoProtocolTestMixin, PluginTestMixin):
+    pass
+
+
+class TestVirtuosoProtocol(TestCase, VirtuosoProtocolTestMixin):
+    """ Tests for virtuoso_protocol plugin. """
+    CONTEXT = "http://github.com/nwebs/surf.virtuoso_protocol#surf_test_graph_dummy2"
+
+    def _create_persons(self, session):
         Person = session.get_class(surf.ns.FOAF + "Person")
         for name in ["John", "Mary", "Jane"]:
             # Some test data.
@@ -43,12 +57,11 @@ class TestVirtuosoProtocol(TestCase):
             person.foaf_name = name
             person.save()
 
-        return store, session
-
     def test_missing_default_context(self):
         """ Test read & write without default_context set. """
 
         _, session = self._get_store_session(use_default_context=False)
+        self._create_persons(session)
 
         Person = session.get_class(surf.ns.FOAF + "Person")
         john = session.get_resource("http://John", Person)
@@ -62,6 +75,8 @@ class TestVirtuosoProtocol(TestCase):
         """ Test owl:sameAs inferencing. """
 
         store, session = self._get_store_session()
+        self._create_persons(session)
+
         # Let's say Jonathan is the same Person as John
         Person = session.get_class(surf.ns.FOAF["Person"])
         john = session.get_resource("http://John", Person)
@@ -79,5 +94,5 @@ class TestVirtuosoProtocol(TestCase):
                             .where((jonathan.subject, surf.ns.FOAF['name'], '?s'))
         r = store.execute_sparql(unicode(query))
 
-        self.assertEquals(set(entry['s'] for entry in r["results"]["bindings"]),
+        self.assertEquals(set(entry['s']['value'] for entry in r["results"]["bindings"]),
                           set([john.foaf_name[0]]))
